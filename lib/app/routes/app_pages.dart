5035e85210e90aa2ec399c07b089e1dbee8d4c7f
alfinsyahr <syahrinalfin105@gmail.com>
2025-02-03 14:48:26 +0700

    Silahkan di Review, Beritahu jika ada kesalahan

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,8 @@ import '../modules/daftar/bindings/daftar_binding.dart';
 import '../modules/daftar/views/daftar_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/masuk/bindings/masuk_binding.dart';
+import '../modules/masuk/views/masuk_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
 
@@ -29,6 +31,11 @@ class AppPages {
       name: _Paths.DAFTAR,
       page: () => const DaftarView(),
       binding: DaftarBinding(),
+    ),
+    GetPage(
+      name: _Paths.MASUK,
+      page: () => const MasukView(),
+      binding: MasukBinding(),
     ),
   ];
 }
